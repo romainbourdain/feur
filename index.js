69c841660105bb9675274459530c8f039b3f055f
bourdain <romain.bourdain@etu.unistra.fr>
2022-10-17 15:38:17 +0200

    v1

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const quoi = require("./quoi.json");
+
+const intents = new Discord.IntentsBitField(3276799);
+const bot = new Discord.Client({ intents });
+
+bot.login(config.BOT_TOKEN);
+
+bot.on("messageCreate", async (message) => {
+  quoi.forEach((q) => {
+    const content = message.content.toLowerCase().replace(/[^a-z]+/g, "");
+    if (
+      content.length >= q.length &&
+      content.slice(content.length - q.length, content.length) === q
+    ) {
+      message.channel.send("feur");
+    }
+  });
+});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.tag} est connecté`);
+});
