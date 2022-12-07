3271f89e0eed5660f65696738b20b3f92bfa5bce
bourdain <romain.bourdain@unistra.fr>
2022-12-07 17:54:37 +0100

    random

--- index.js
+++ index.js
@@ -5,18 +5,28 @@ require("dotenv").config();
 const intents = new Discord.IntentsBitField(3276799);
 const bot = new Discord.Client({ intents });
 
+const imageList = [
+  "https://media.tenor.com/CJIntL3axZUAAAAd/feur-meme.gif",
+  "https://media.tenor.com/XH3pihjGqkwAAAAC/feur-quoi.gif",
+  "https://media.tenor.com/MKwpJdV9qrUAAAAM/feur-heart-locket.gif",
+  "https://media.tenor.com/zrP8LiC-940AAAAd/feur-phoenix-wright.gif",
+  "https://media.tenor.com/71BKREg8HCMAAAAd/feur-bambam-feur.gif",
+  "https://media.tenor.com/MlnMzLYdEnEAAAAC/multicort-feur.gif",
+  "https://media.tenor.com/lGR06JUFwikAAAAC/cocrane-feur.gif",
+];
+
 bot.login(process.env.BOT_TOKEN);
 
 bot.on("messageCreate", async (message) => {
   quoi.forEach((q) => {
     const content = message.content.toLowerCase().replace(/[^a-z]+/g, "");
+    const image = imageList[parseInt(Math.random() * 7)];
     if (
       content.length >= q.length &&
       content.slice(content.length - q.length, content.length) === q
     ) {
-      const embed = new Discord.EmbedBuilder()
-        .setTitle("Feur")
-        .setImage("https://media.tenor.com/CJIntL3axZUAAAAd/feur-meme.gif");
+      (Math.random() * 5) % 5;
+      const embed = new Discord.EmbedBuilder().setTitle("Feur").setImage(image);
       message.channel.send({ embeds: [embed] });
     }
   });
